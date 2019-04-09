5328009ea09fea376747d93bbd4521c66d45ee1b
jessica <jessgauvin@hotmail.com>
2019-04-09 11:16:02 -0400

    on avance

--- TP7/Tp9_IFT3205_3-3.c
+++ TP7/Tp9_IFT3205_3-3.c
@@ -43,6 +43,20 @@ void showRsbResult(float rsb, char* message){
   printf("\n");
 }
 
+void restoreCanal(float* Sign, float* SignResult, int length){
+  SignResult[0] = -Sign[0]/2.0;
+  SignResult[1] = -Sign[1]/2.0+Sign[0]+17.0*SignResult[0]/6.0;
+  SignResult[2] = -Sign[2]/2.0+Sign[1]+17.0*SignResult[1]/6.0-11.0*SignResult[0]/6.0;
+
+  
+
+
+  for(int i = 3; i<length; i++){
+    SignResult[i] = -Sign[i]/2.0+Sign[i-1]+17.0/6.0*SignResult[i-1]-11.0*SignResult[i-2]/6.0+SignResult[i-3]/3.0;
+    //printf("%d. result: %f\n",i, SignResult[i] );
+  }
+}
+
 /*------------------------------------------------*/
 /* PROGRAMME PRINCIPAL   -------------------------*/                     
 /*------------------------------------------------*/
@@ -59,7 +73,7 @@ int main(int argc,char **argv)
    float*  Sign=LoadSignalDat("SoundFileInCanal",&length);
    float* SignRestore=fmatrix_allocate_1d(length);
 
-   //TODO: restore and calculate rsb
+   restoreCanal(Sign, SignRestore, length);
 
 
    showRsbResult(rsbCalculation(SignRestore, length), "RSB=");
